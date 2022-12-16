e936db9335750abf94e1faf3ebad3f82be4a0968
qchen71 <qchen71@ford.com>
2022-12-16 16:12:37 +0800

    代码清单1-1 代码清单1-2 使用Netty开发一个简单的HTTP服务器 当向服务器发送请求时响应helloworld

---
+++ src/main/java/io/huhu/netty/demo/HttpHelloWorldServer.java
@@ -0,0 +1,48 @@
+package io.huhu.netty.demo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpServerExpectContinueHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import static io.netty.handler.logging.LogLevel.INFO;
+
+public class HttpHelloWorldServer {
+
+    public static void main(String[] args) throws Exception {
+        // 配置服务器
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap();
+            serverBootstrap.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(INFO))
+                    .childHandler(new ChannelInitializer<Channel>() {
+                        @Override
+                        protected void initChannel(Channel ch) throws Exception {
+                            ChannelPipeline pipeline = ch.pipeline();
+                            pipeline.addLast(new HttpServerCodec());
+                            pipeline.addLast(new HttpServerExpectContinueHandler());
+                            pipeline.addLast(new HttpHelloWorldServerHandler());
+                        }
+                    });
+            // 启动服务器
+            Channel channel = serverBootstrap.bind(8080).sync().channel();
+            System.err.println("Open your web browser and navigate to http://127.0.0.1:8080");
+            channel.closeFuture().sync();
+        }
+        finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+
+}
---
+++ src/main/java/io/huhu/netty/demo/HttpHelloWorldServerHandler.java
@@ -0,0 +1,35 @@
+package io.huhu.netty.demo;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.HttpObject;
+import io.netty.handler.codec.http.HttpRequest;
+
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_LENGTH;
+import static io.netty.handler.codec.http.HttpHeaderNames.CONTENT_TYPE;
+import static io.netty.handler.codec.http.HttpHeaderValues.TEXT_PLAIN;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+
+public class HttpHelloWorldServerHandler extends SimpleChannelInboundHandler<HttpObject> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, HttpObject msg) throws Exception {
+        if (msg instanceof HttpRequest) {
+            HttpRequest request = (HttpRequest) msg;
+            DefaultFullHttpResponse response = new DefaultFullHttpResponse(
+                    request.protocolVersion(), OK, Unpooled.wrappedBuffer("helloworld".getBytes()));
+            response.headers()
+                    .set(CONTENT_TYPE, TEXT_PLAIN)
+                    .setInt(CONTENT_LENGTH, response.content().readableBytes());
+            ChannelFuture channelFuture = ctx.write(response);
+        }
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        ctx.flush();
+    }
+}
